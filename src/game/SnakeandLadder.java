2462d88a85b10c802a68e44bd15208856702e596
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-02 10:36:59 +0530

    got the value after die rolled

--- src/game/SnakeandLadder.java
+++ src/game/SnakeandLadder.java
@@ -5,6 +5,7 @@ public class SnakeandLadder {
 		System.out.println("Welcome to Snake and Ladders program");
 		int playerposition = 0;
 		System.out.println("player start position is:" +playerposition);
+		int die_rolled = (int) (Math.floor(Math.random() * 10) % 6+1);
+		System.out.println("The rolled die value is:" +die_rolled);
 	}
-
 }
