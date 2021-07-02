b80423842d275f47179cddf0a7898bbdc8a7a7fd
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-02 11:34:48 +0530

    remove the dive value if player got morethan 100

--- src/game/SnakeandLadder.java
+++ src/game/SnakeandLadder.java
@@ -9,22 +9,25 @@ public class SnakeandLadder {
 		while (Winning_Position > player_position) {
 			int die_rolled = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 			System.out.println("The rolled die value is:" + die_rolled);
+			int Remaining_Position = Winning_Position - player_position;
 			int option = (int) (Math.floor(Math.random() * 10) % 3);
 			System.out.println("Selected option is:" + option);
-			switch (option) {
-			case 0:
-				System.out.println("No play");
-				player_position += 0;
-				break;
-			case 1:
-				System.out.println("Ladder");
-				player_position += die_rolled;
-				break;
-			default:
-				System.out.println("Snake");
-				player_position -= die_rolled;
-				if (player_position < 0)
-					player_position = 0;
+			if (Remaining_Position >= die_rolled) {
+				switch (option) {
+				case 0:
+					System.out.println("No play");
+					player_position += 0;
+					break;
+				case 1:
+					System.out.println("Ladder");
+					player_position += die_rolled;
+					break;
+				default:
+					System.out.println("Snake");
+					player_position -= die_rolled;
+					if (player_position < 0)
+						player_position = 0;
+				}
 			}
 		}
 		System.out.println("player position is:" + player_position);
